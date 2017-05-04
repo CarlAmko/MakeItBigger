4cde3568b020cc449fb5201759e88c71d9f2c762
Carl Amko <carl@carlamko.me>
2017-05-04 12:35:13 -0400

    refactor: Moved activity fragment from app build to Android Joke library

--- jokeproviderlibrary/src/main/java/com/example/android/jokeproviderlibrary/JokeFragment.java
+++ jokeproviderlibrary/src/main/java/com/example/android/jokeproviderlibrary/JokeFragment.java
@@ -1,4 +1,4 @@
-package com.udacity.gradle.builditbigger;
+package com.example.android.jokeproviderlibrary;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,32 +8,26 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.example.android.jokeproviderlibrary.EndpointAsyncTask;
-
 import timber.log.Timber;
 
-
-/**
- * A placeholder fragment containing a simple view.
- */
-public class MainActivityFragment extends Fragment implements EndpointAsyncTask.EndpointResponseListener {
+public class JokeFragment extends Fragment implements EndpointAsyncTask.EndpointResponseListener {
 
     Button getJokeButton;
-    public MainActivityFragment() {}
+    public JokeFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_joke, container, false);
 
-        getJokeButton = (Button) root.findViewById(com.example.android.jokeproviderlibrary.R.id.b_joke);
+        getJokeButton = (Button) root.findViewById(R.id.b_joke);
         Timber.d(getJokeButton.toString());
 
         getJokeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Timber.d("Joke button clicked!");
-                new EndpointAsyncTask().execute(MainActivityFragment.this);
+                new EndpointAsyncTask().execute(JokeFragment.this);
             }
         });
 
